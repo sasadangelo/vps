171ee3eebc524b47575bce04f7aa6ce57e61e3d1
Salvatore D'Angelo <sdangelo@it.ibm.com>
2018-12-03 19:39:13 +0100

    Added theme support

--- wp-install/configure.sh
+++ wp-install/configure.sh
@@ -19,7 +19,7 @@ WP_PASSWD="password"
 WP_USER_EMAIL="user@gmail.com"
 WP_NAME="My Website"
 WP_LOCALE="it_IT"
-WP_THEME="storefront"
+WP_THEME="twentyseventeen"
 WP_PLUGINS="antispam-bee,better-font-awesome,contact-form-7,cookie-law-info, \
 	php-code-widget,re-add-underline-justify,custom-css-js, \
 	social-media-widget,w3-total-cache,widget-logic,widgets-on-pages, \
